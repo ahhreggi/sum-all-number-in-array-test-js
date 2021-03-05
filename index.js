a0e4fec6961b1e0f93735b8cb64369f184e0c57f
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-05 19:51:18 +0000

    Implement sumItems

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = function(array) {
+  if (array.length === 0) {
+    return 0;
+  } else {
+    let first = array[0];
+    if (Array.isArray(first)) {
+      first = sumItems(first);
+    }
+    return first + sumItems(array.slice(1));
+  }
+};
 
 module.exports = sumItems;
